062d616d8171338320005fafb988e1f25d66682c
bestfriend1 <shamimhossain5037@gmail.com>
2023-06-21 00:03:54 +0600

    added

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const btn = document.getElementById('btn');
+const icon = document.querySelector('#btn i');
+const outputBox = document.getElementById('output');
+
+function spech() {
+    if (icon.classList.contains('fa-play')) {
+        icon.classList.remove('fa-play')
+        icon.classList.add('fa-pause')
+    }
+    if (window.webkitSpeechRecognition || window.SpeechRecognition) {
+        let speechRecognition = new webkitSpeechRecognition();
+        speechRecognition.continuous = true;
+        speechRecognition.interimResults = true;
+        speechRecognition.lang = 'en-GB';
+        speechRecognition.onresult = (event) => {
+            if(event){
+                outputBox.textContent = event.results[0][0].transcript;
+                icon.classList.remove('fa-pause')
+                icon.classList.add('fa-play')
+            }
+            
+        }
+
+    }
+}
+
+btn.onclick = () => {
+    spech();
+}
